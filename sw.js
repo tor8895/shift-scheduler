b5d0b4c20269201b276ca2f509b6b76841e642ea
tor8895 <tor8895@gmail.com>
2026-01-14 11:37:54 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+// 注意：這裡的版本號名稱跟之前的不同，確保獨立運作
+const CACHE_NAME = 'shift-scheduler-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
